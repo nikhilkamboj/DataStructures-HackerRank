3d52e0834109e93d6d89e8a54c99624508822839
Nikhil Kamboj <n.kamboj15@gmail.com>
2018-02-05 11:47:55 +0530

    added insertAtHead method

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -4,6 +4,24 @@ public class LinkedList {
     Node head;
 
 
+
+
+    public void insertNode(int data){
+//  inserting new node at the head.
+        Node newNode = new Node();
+        newNode.setData(data);
+        newNode.setNext(null);
+
+        if (head == null){
+            head = newNode;
+        } else {
+            newNode.setNext(head);
+            head = newNode;
+        }
+    }
+
+
+
     public boolean haveLoop() {
         Node fast = head.getNext();
         Node slow = head;
