78d08eef84cee1f7e0e641fba4c9e2c1618a95a1
Nikhil Kamboj <n.kamboj15@gmail.com>
2018-02-21 12:46:17 +0530

    bubble sort swap added with desc order

--- src/sorting_algos/BubbleSort.java
+++ src/sorting_algos/BubbleSort.java
@@ -3,27 +3,41 @@ package sorting_algos;
 public class BubbleSort {
 
     public void sortAsc(int[] array) {
-        boolean isSorted = false;
         int arrayLength = array.length;
-        int temp = 0;
 
         for (int i = 0; i < arrayLength-1; i++) {
             for (int j = 0; j < arrayLength-i-1; j++) {
                 if (array[j] > array[j+1]) {
-                    temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                  swap(array,j,j+1);
+                }
+            }
+        }
+    }
+
+    public void sortDesc(int[] array) {
+        int arrayLength = array.length;
+
+        for (int i = 0; i < arrayLength-1; i++) {
+            for (int j = 0; j < arrayLength-i-1; j++) {
+                if (array[j] < array[j+1]) {
+                    swap(array,j,j+1);
                 }
             }
         }
 
     }
 
+    private void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
 
     public static void main(String[] args) {
         BubbleSort bubbleSort = new BubbleSort();
         int[] array = {9,2,4,7,8,1,-1,50};
-        bubbleSort.sortAsc(array);
+        bubbleSort.sortDesc(array);
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
